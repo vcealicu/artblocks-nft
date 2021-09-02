add12c16199d7d5df20d3aea97c83b9fe4a24f3b
Vlad Cealicu <vcealicu@cryptocompare.com>
2021-09-02 07:19:50 +0100

    Update cc-ms-artblocks-nft.js

--- cc-ms-artblocks-nft/cc-ms-artblocks-nft.js
+++ cc-ms-artblocks-nft/cc-ms-artblocks-nft.js
@@ -9,6 +9,10 @@ let scriptParams = scriptArgs.parse({
     IP: '0.0.0.0',
 });
 
+function isValidHex(inputString) {
+  return inputString.match(/^0x[0-9a-f]+$/i) !== null;
+}
+
 apiCoreMs.addEndpoint({
     CacheLength: 0,
     HttpVerb: 'GET',
@@ -17,16 +21,32 @@ apiCoreMs.addEndpoint({
     Description: '',
     Examples: [],
     execute: function(callParams, hitTimestamp, executeCallback) {
-        let seed = callParams.seed || '0x' + crypto.randomBytes(20).toString("hex");
+        let seed = '0x' + crypto.randomBytes(20).toString("hex");
+        if(callParams.seed === undefined && callParams.seed.length>10){
+            if ( isValidHex(callParams.seed) ){
+                seed = callParams.seed;
+            } else {
+                callParams.seed = undefined;
+            }
+        }
+        let seed = callParams.seed || ;
         let resourcesToPreload = {
             seed: () => {
                 return seed;
             },
             windowHeight: () => {
-                return callParams.height !== undefined ? parseInt(callParams.height, 10) : 1200;
+                let height = callParams.height !== undefined ? parseInt(callParams.height, 10) : 1200;
+                if( height<600  || height>1200){
+                    height = 1200;
+                }
+                return height;
             },
             windowWidth: () => {
-                return callParams.width !== undefined ? parseInt(callParams.width, 10) : 1200;
+                let width = callParams.width !== undefined ? parseInt(callParams.height, 10) : 1200;
+                if( width<600  || width>1200){
+                    width = 1200;
+                }
+                return width;
             },
             canvasBuffer: () => {
                 return function(dataBuffer) {
